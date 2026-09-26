e9457ab1c13afa26221b03155223a7284468f7aa
Muhammad Huzaifa <mhhuzaifa223@gmail.com>
2026-09-25 20:25:41 -0400

    Test CI failure handling

--- Day-03-CI-CD/Project-22/test_app.py
+++ Day-03-CI-CD/Project-22/test_app.py
@@ -2,7 +2,7 @@ from app import add, subtract, multiply, divide
 import pytest
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 6
 
 def test_subtract():
     assert subtract(5, 3) == 2
